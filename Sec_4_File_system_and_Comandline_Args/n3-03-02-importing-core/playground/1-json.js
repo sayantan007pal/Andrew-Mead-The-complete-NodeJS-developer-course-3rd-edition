af2649911c6359bf060fda14b9edc433d9eb3572
Sayantan Pal <sayantanpal100@LHPVTGPW0W.local>
2025-08-14 18:23:46 +0530

    Sec_4_File_system_and_Comandline_Args --[#2]--17.Storing data with JSON

--- Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/playground/1-json.js
+++ Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/playground/1-json.js
@@ -1,28 +1,25 @@
 const fs = require('fs') // Importing the file system module
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  year: 2016
-}
-const bookJSON = JSON.stringify(book) // Convert object to JSON string
-console.log(bookJSON)
 
-/*
-☁  playground [main] ⚡  node ./1-json.js                                                   
-{"title":"Ego is the Enemy","author":"Ryan Holiday","year":2016}
-*/
-const parsedBook = JSON.parse(bookJSON) // Convert JSON string back to object
-console.log(parsedBook)
-console.log('TITLE: ' + parsedBook.title)
-console.log('AUTHOR: ' + parsedBook.author)
-console.log('YEAR: ' + parsedBook.year)
+const dataBuffer = fs.readFileSync('1-json.json') // Reading the JSON file synchronously
+console.log(dataBuffer) // Logging the buffer data
+
+const dataJSON = dataBuffer.toString() // Converting buffer to string
+console.log(dataJSON) // Logging the JSON string
+
+
+const book = JSON.parse(dataJSON) // Parsing JSON string to object
+console.log(book)
+console.log(book.title)
+console.log(book.author)
+console.log(book.year)
+
 
 /*
 ☁  playground [main] ⚡  node ./1-json.js
+<Buffer 7b 22 74 69 74 6c 65 22 3a 22 45 67 6f 20 69 73 20 74 68 65 20 45 6e 65 6d 79 22 2c 22 61 75 74 68 6f 72 22 3a 22 52 79 61 6e 20 48 6f 6c 69 64 61 79 ... 14 more bytes>
+{"title":"Ego is the Enemy","author":"Ryan Holiday","year":2016}
 { title: 'Ego is the Enemy', author: 'Ryan Holiday', year: 2016 }
-TITLE: Ego is the Enemy
-AUTHOR: Ryan Holiday
-YEAR: 2016
+Ego is the Enemy
+Ryan Holiday
+2016
 */
-
-fs.writeFileSync('1-json.json', bookJSON) // Write JSON string to a file
