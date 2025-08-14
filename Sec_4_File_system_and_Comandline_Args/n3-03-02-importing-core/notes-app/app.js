4639246f96488ded1d8633f331a0b3494bae1c67
Sayantan Pal <sayantanpal100@LHPVTGPW0W.local>
2025-08-14 16:48:43 +0530

    Sec_4_File_system_and_Comandline_Args --16.Argument parsing with yeargs: PART 2

--- Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/notes-app/app.js
+++ Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/notes-app/app.js
@@ -1,67 +1,32 @@
-const add = require('./utils')
-const getNotes = require('./notes')
+
 const yargs = require('yargs')
 
 //Crete add command
 yargs.command({
   command : 'add',
   description : 'Add a new Note',
-  handler : function (){
-    console.log('Printing new note for adding.')
+  builder:{
+    title:{
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body:{
+      describe:'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler : function (argv){
+    console.log('TITLE: '+ argv.title)
+    console.log('BODY: '+ argv.body)
   }
 
 })
-//Create remove command
- yargs.command({
-  command : 'remove',
-  description : 'Remove a Note',
-  handler : function (){
-    console.log('Printing new note for removing.')
-  }
-
-
-})
-
-//Create List command
-yargs.command({
-  command:'list',
-  description: 'List all Notes',
-  handler: function(){
-    console.log('printing the whole list of notes')
-  }
-})
-
-
-
-console.log(yargs.argv)
+yargs.parse()
 
 /*
-
-☁  notes-app [main] ⚡  node app.js --help                     
-utils.js is running
-app.js [command]
-
-Commands:
-  app.js add     Add a new Note
-  app.js remove  Remove a Note
-
-Options:
-  --help     Show help                                                 [boolean]
-  --version  Show version number                                       [boolean]
-*/
-
-
-/*
-  ☁  notes-app [main] ⚡  node app.js add                        
-utils.js is running
-Printing new note for adding.
-{ _: [ 'add' ], '$0': 'app.js' }
-*/
-
-/*
-☁  notes-app [main] ⚡  node app.js remove
-utils.js is running
-Printing new note for removing.
-{ _: [ 'remove' ], '$0': 'app.js' }
-
+☁  notes-app [main] ⚡  node app.js add --title="this is my title" --body="this is my body"
+TITLE: this is my title
+BODY: this is my body
 */
