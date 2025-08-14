756b4d158ad3d31dae852883165827c6fa3456e1
Sayantan Pal <sayantanpal100@LHPVTGPW0W.local>
2025-08-14 16:19:08 +0530

    Sec_4_File_system_and_Comandline_Args ---[#2]--16.Argument parsing with yeargs: PART 1

--- Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/notes-app/app.js
+++ Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/notes-app/app.js
@@ -2,28 +2,66 @@ const add = require('./utils')
 const getNotes = require('./notes')
 const yargs = require('yargs')
 
-console.log(process.argv)
+//Crete add command
+yargs.command({
+  command : 'add',
+  description : 'Add a new Note',
+  handler : function (){
+    console.log('Printing new note for adding.')
+  }
+
+})
+//Create remove command
+ yargs.command({
+  command : 'remove',
+  description : 'Remove a Note',
+  handler : function (){
+    console.log('Printing new note for removing.')
+  }
+
+
+})
+
+//Create List command
+yargs.command({
+  command:'list',
+  description: 'List all Notes',
+  handler: function(){
+    console.log('printing the whole list of notes')
+  }
+})
+
+
+
 console.log(yargs.argv)
 
 /*
-☁  notes-app [main] ⚡  node app.js                            
+
+☁  notes-app [main] ⚡  node app.js --help                     
 utils.js is running
-[
-  '/usr/local/bin/node',
-  '/Users/sayantanpal100/Desktop/Udemy Projects/Andrew-Mead-The-complete-NodeJS-developer-course-3rd-edition/Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/notes-app/app.js'
-]
-{ _: [], '$0': 'app.js' }
+app.js [command]
+
+Commands:
+  app.js add     Add a new Note
+  app.js remove  Remove a Note
+
+Options:
+  --help     Show help                                                 [boolean]
+  --version  Show version number                                       [boolean]
 */
 
 
 /*
-☁  notes-app [main] ⚡  node app.js add --title="Things to buy"
+  ☁  notes-app [main] ⚡  node app.js add                        
 utils.js is running
-[
-  '/usr/local/bin/node',
-  '/Users/sayantanpal100/Desktop/Udemy Projects/Andrew-Mead-The-complete-NodeJS-developer-course-3rd-edition/Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/notes-app/app.js',
-  'add',
-  '--title=Things to buy'
-]
-{ _: [ 'add' ], title: 'Things to buy', '$0': 'app.js' }
+Printing new note for adding.
+{ _: [ 'add' ], '$0': 'app.js' }
+*/
+
+/*
+☁  notes-app [main] ⚡  node app.js remove
+utils.js is running
+Printing new note for removing.
+{ _: [ 'remove' ], '$0': 'app.js' }
+
 */
