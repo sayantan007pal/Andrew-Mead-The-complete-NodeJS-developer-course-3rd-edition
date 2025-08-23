b7ef155d85854289774a8da60aa6ca79e349a6a9
Sayantan Pal <sayantanpal100@Mac.Vortex.local>
2025-08-24 03:17:36 +0530

    Sec_4_File_system_and_Comandline_Args --#3--21.ES6 Aside: Arrow function

--- Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/playground/2-Arrow-function.js
+++ Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/playground/2-Arrow-function.js
@@ -1,18 +1,22 @@
-//Object "event" with name and printGuestList method
-const event = {
-     name : "Birthday Party",
-     printGuestList: function () {
-        console.log("Guest list for : " + this.name); // 'this' is a binding to the event object
-     }
-}
-event.printGuestList()
-
 
 const eventArrow = {
     name: "Birthday Party of arrow function",
+
     printGuestList: () => {
         console.log("Guest list for : " + eventArrow.name); // 'this' is not bound in arrow functions so we should use the eventArrow object
         console.log("Guest list for : " + this.name);
     }
 }
 eventArrow.printGuestList()
+
+// so in order to access "this" we need to use a regular function
+
+const event = {
+    name: "Birthday Party of arrow function",
+    
+    printGuestList() {
+        // console.log("Guest list for : " + eventArrow.name); // 'this' is not bound in arrow functions so we should use the eventArrow object
+        console.log("Guest list for : " + this.name);
+    }
+}
+event.printGuestList()
