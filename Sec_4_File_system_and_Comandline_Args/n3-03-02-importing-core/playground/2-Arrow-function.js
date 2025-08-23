0a20d4ed349cf2c2d4861a04b11429583f200ee3
Sayantan Pal <sayantanpal100@Mac.Vortex.local>
2025-08-24 03:11:41 +0530

    Sec_4_File_system_and_Comandline_Args --#2--21.ES6 Aside: Arrow function

--- Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/playground/2-Arrow-function.js
+++ Sec_4_File_system_and_Comandline_Args/n3-03-02-importing-core/playground/2-Arrow-function.js
@@ -1,12 +1,18 @@
-// //normal function
-// const square = function(x){
-//     return x * x;
-// }
-
-
-//arrow function
-const square = (x) => {
-    return x * x;
+//Object "event" with name and printGuestList method
+const event = {
+     name : "Birthday Party",
+     printGuestList: function () {
+        console.log("Guest list for : " + this.name); // 'this' is a binding to the event object
+     }
 }
+event.printGuestList()
 
-console.log(square(8));
+
+const eventArrow = {
+    name: "Birthday Party of arrow function",
+    printGuestList: () => {
+        console.log("Guest list for : " + eventArrow.name); // 'this' is not bound in arrow functions so we should use the eventArrow object
+        console.log("Guest list for : " + this.name);
+    }
+}
+eventArrow.printGuestList()
