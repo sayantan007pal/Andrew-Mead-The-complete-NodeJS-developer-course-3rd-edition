f6bf1721cb0a4b90b4b0730afaba82a219e65af3
Sayantan Pal <sayantanpal100@Mac.Vortex.local>
2025-08-28 03:15:37 +0530

    Sec_6_Asynchronous_Node.js(Weather_app)---32.Customizing HTTP requests

--- Sec_6_Asynchronous_Node.js(Weather_app)/weather-app/app.js
+++ Sec_6_Asynchronous_Node.js(Weather_app)/weather-app/app.js
@@ -1,9 +1,13 @@
 const request = require('request');
 
-const url = 'https://api.weatherstack.com/current?access_key=d559e719faab07310756a2f1c6c6967d&query=Kolkata';
+const url = 'https://api.weatherstack.com/current?access_key=d559e719faab07310756a2f1c6c6967d&query=Kolkata&units=m';
 
-request({url:url}, (error,response)=> {
-    const data = JSON.parse(response.body);
-    console.log("whole response : ",data)
-    console.log("Current :", data.current)
+request({url:url, json:true}, (error,response)=> {  // json:true means telling it is already in json format so no need to parse
+    const data = response.body;
+
+    console.log(data.current.weather_descriptions[0] +' It is currently ' + data.current.temperature + ' degrees out. It feels like ' + data.current.feelslike +' degrees out.');
 })
+
+/* Goal: print a small forcust for the user
+print : weather_descriptions +"It is currently 20 degrees out. feels like 22 degrees out"
+*/
